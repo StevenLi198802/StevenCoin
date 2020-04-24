d35f406bd58baa9b1774b3d9909364b55a83e456
Steven Li <stevenli198802@gmail.com>
2020-04-24 19:36:56 +0900

    master: Created the simple blockchain

---
+++ main.js
@@ -0,0 +1,71 @@
+const SHA256 = require('crypto-js/sha256');
+
+class Block{
+    constructor(index, timestamp, data, previousHash = ''){
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = this.calculateHash();
+        this.nonce = 0;
+    }
+
+    calculateHash(){
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+    }
+
+    mineBlock(difficulty){
+        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")){
+            this.nonce++;
+            this.hash == this.calculateHash();
+        }
+
+        console.log("Block mined: " + this.hash);
+    }
+}
+
+class BlockChain{
+    constructor(){
+        this.chain = [this.createGenesisBlock()];
+        this.difficulty = 1;
+    }
+
+    createGenesisBlock(){
+        return new Block(0, "01/02/2019", "This is the genesis block", 0);
+    }
+
+    getLatestBlock(){
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(newBlock){
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.mineBlock(this.difficulty);
+        this.chain.push(newBlock);
+    }
+
+    isChainValid(){
+        for(let i = 1; i < this.chain.length; i++){
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+
+            if(currentBlock.hash != currentBlock.calculateHash()){
+                return false;
+            }
+
+            if(currentBlock.previousHash != previousBlock.hash){
+                return false;
+            }
+
+            return false;
+        }
+    }
+}
+
+let stevenCoin = new BlockChain();
+
+console.log("Mine block 1...");
+stevenCoin.addBlock(new Block(1, "01/03/2019", { amount:1 }));
+
+console.log("Mine block 2...")
+stevenCoin.addBlock(new Block(2, "01/04/2019", { amount:2 }));
